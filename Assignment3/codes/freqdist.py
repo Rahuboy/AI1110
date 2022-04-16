e8a1a39d1a880e9ddd17aac51fa60e5af695fd11
Rahuboy <102507616+Rahuboy@users.noreply.github.com>
2022-04-16 13:41:32 +0530

    Update freqdist.py

--- Assignment3/codes/freqdist.py
+++ Assignment3/codes/freqdist.py
@@ -9,7 +9,7 @@ raw_data = np.array(read)
 bin = [84+2*i for i in range(9)] 
 class_intervals = ["{}-{}".format(bin[i],bin[i+1]) for i in range(8)]
 
-#using inbuilt function for frequencies
+#using inbuilt function for frequencies / class indices
 hist = np.histogram(raw_data,bins=bin)
 print(hist[0])
 
