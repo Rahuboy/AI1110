a1f9250faf1e2e26ce080636e73e471035640410
Rahuboy <102507616+Rahuboy@users.noreply.github.com>
2022-04-23 15:23:15 +0530

    Update probabilities.py

--- Assignment4/codes/probabilities.py
+++ Assignment4/codes/probabilities.py
@@ -5,27 +5,28 @@ import pandas as pd
 import numpy as np
 
 #function to find probabilities
-def prob(x,list):
-    return list[x]/sum(list.values())
+def prob(x,list1):
+    return list1[x]/sum(list1.values())
 
 if __name__ == "__main__":
 
     wb = load_workbook("tables/raw_data.xlsx").active
 
 #reading data
-    for items in wb.iter_rows(min_row=1, max_row=1, values_only=True):
-        pass
 
-    for freq in wb.iter_rows(min_row=2, max_row=2, values_only=True):
-        pass
+    items = [i for i in wb.iter_rows(min_row=1, max_row=1, values_only=True)]
 
-    list = dict((x,y) for x,y in zip(items,freq))
+    print(items)
+    
+    freq = [j for j in wb.iter_rows(min_row=2, max_row=2, values_only=True)]
+
+    list1 = dict((x,y) for x,y in zip(items[0],freq[0]))
         
 #creating list of probabilities
-    problist = np.array([prob(i,list) for i in range(10)])
+    problist = np.array([prob(i,list1) for i in range(10)])
 
 #printing  theoretical probability value
-    print("probability of occurrence of 6 is ",prob(6,list))
+    print("probability of occurrence of 6 is ",prob(6,list1))
 
 #Sample size
     N = 2500
