812bc854b9b5f3448586a4372d6e4b87c5c2d544
Rahuboy <102507616+Rahuboy@users.noreply.github.com>
2022-04-25 16:16:45 +0530

    Update probabilities.py

--- Assignment4/codes/probabilities.py
+++ Assignment4/codes/probabilities.py
@@ -1,45 +1,41 @@
 #code to find theoretical values of probabilities, and write data to an excel file.
 
-from openpyxl import load_workbook
 import pandas as pd
 import numpy as np
 
-#function to find probabilities
-def prob(x,list1):
-    return list1[x]/sum(list1.values())
+# function to find probabilities
+def prob(x,dict1):
+    return dict1[x]/sum(dict1.values())
 
-if __name__ == "__main__":
 
-    wb = load_workbook("tables/raw_data.xlsx").active
 
 #reading data
 
-    items = [i for i in wb.iter_rows(min_row=1, max_row=1, values_only=True)]
+df = pd.read_excel("tables/raw_data.xlsx");
 
-    print(items)
-    
-    freq = [j for j in wb.iter_rows(min_row=2, max_row=2, values_only=True)]
+items = df["Digit"]
+frequency = df["Frequency"]
 
-    list1 = dict((x,y) for x,y in zip(items[0],freq[0]))
-        
-#creating list of probabilities
-    problist = np.array([prob(i,list1) for i in range(10)])
+#creating arrays
+itemarray = [i for i in items]
+freqarray = [i for i in frequency]
 
-#printing  theoretical probability value
-    print("probability of occurrence of 6 is ",prob(6,list1))
+#creating dictionary and probability list
+dict1 = dict((x,y) for x,y in zip(itemarray,freqarray))
 
-#Sample size
-    N = 2500
+problist = np.array([prob(i,dict1) for i in range(10)])
 
-    counter = 0
+# Sample size
+N = 10000
+
+counter = 0
 
 
-#finding empirical value of probability, by counting frequency
-    for i in range(N):
-        x = np.random.choice([i for i in range(10)], p = problist)
-        if(x == 6):
-            counter = counter+1
+# finding empirical value of probability, by counting frequency
+for i in range(N):
+    x = np.random.choice([i for i in range(10)], p = problist)
+    if(x == 6):
+        counter = counter+1
 
-#printing empirical value
-    print("Experimental value of probability of occurrence of 6 is",counter/N)
-    
+# printing empirical value
+print("Experimental value of probability of occurrence of 6 is",counter/N)
