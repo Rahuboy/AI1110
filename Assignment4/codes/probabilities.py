bd078e0ba82911de9d18bc1d83e2254948b4ab27
Rahuboy <102507616+Rahuboy@users.noreply.github.com>
2022-04-26 21:13:52 +0530

    Update probabilities.py

--- Assignment4/codes/probabilities.py
+++ Assignment4/codes/probabilities.py
@@ -1,29 +1,5 @@
-#code to find theoretical values of probabilities, and write data to an excel file.
-
-import pandas as pd
+#code to find theoretical values of probability, assuming uniform distribution
 import numpy as np
-
-# function to find probabilities
-def prob(x,dict1):
-    return dict1[x]/sum(dict1.values())
-
-
-
-#reading data
-
-df = pd.read_excel("tables/raw_data.xlsx");
-
-items = df["Digit"]
-frequency = df["Frequency"]
-
-#creating arrays
-itemarray = [i for i in items]
-freqarray = [i for i in frequency]
-
-#creating dictionary and probability list
-dict1 = dict((x,y) for x,y in zip(itemarray,freqarray))
-
-problist = np.array([prob(i,dict1) for i in range(10)])
 
 # Sample size
 N = 10000
@@ -31,9 +7,10 @@ N = 10000
 counter = 0
 
 
-# finding empirical value of probability, by counting frequency
+# finding empirical value of probability, by counting frequency. 
+# Uniform distribution assumed.
 for i in range(N):
-    x = np.random.choice([i for i in range(10)], p = problist)
+    x = np.random.choice([i for i in range(10)])
     if(x == 6):
         counter = counter+1
 
