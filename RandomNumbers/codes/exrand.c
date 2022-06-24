297821b131816f24be7169e2f9cb2b9f85ba343b
Rahuboy <rahulramach3000@gmail.com>
2022-06-24 22:46:48 +0530

    pushing random numbers codes and figures

---
+++ RandomNumbers/codes/exrand.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int  main(void) //main function begins
+{
+ 
+//Uniform random numbers
+uniform("uni.dat", 1000000);
+
+//Gaussian random numbers
+gaussian("gau.dat", 1000000);
+
+//Logarithmic random numbers
+logarithmic("log.dat");
+
+//Mean of uniform
+printf("%lf\n",mean("gau.dat"));
+printf("%lf",variance("gau.dat"));
+return 0;
+}
+
+
